98895f54e2292bc91eb32f4adc5cbb900dfbcf28
tonderflash <g594@hotmail.com>
2025-05-02 02:14:29 -0400

    a

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -14,8 +14,7 @@ data "aws_iam_policy_document" "tf_backend" {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:GetObjectVersion"]
     resources = [
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-*",
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-*/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/*",
     ]
   }
   statement {
