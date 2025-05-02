17a419abd0266efaa61f2f0c93cb6bf5f10221a2
tonderflash <g594@hotmail.com>
2025-05-02 02:08:24 -0400

    Add missing comma in S3 bucket ARN resource list for IAM policy

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -15,7 +15,7 @@ data "aws_iam_policy_document" "tf_backend" {
     actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:GetObjectVersion"]
     resources = [
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-*",
-      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-*/*"
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-*/*",
     ]
   }
   statement {
